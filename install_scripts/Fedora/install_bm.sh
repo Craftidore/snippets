c074c7cceb9ebc63052b0b35b54e91d023426a5f
Craftidore <elliot@madbear.com>
2023-12-28 10:19:36 -0600

    Fix issue with  not existing

--- install_scripts/Fedora/install_bm.sh
+++ install_scripts/Fedora/install_bm.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/sh
 
-rpm -qs fzf || sudo dnf install fzf
+rpm -qs fzf &>/dev/null || sudo dnf install fzf
+ls $HOME/.local/bin &>/dev/null || mkdir -p $HOME/.local/bin
 curl -sS https://raw.githubusercontent.com/madx/bm/main/bm > $HOME/.local/bin/bm
 chmod +x $HOME/.local/bin/bm
