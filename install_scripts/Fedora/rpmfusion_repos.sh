4af737012abc70e388be1d592dcf6a2296b2338c
Craftidore <elliot@madbear.com>
2023-12-28 10:19:58 -0600

    rpmfusion repo script (taken from rpmfusion.org)

---
+++ install_scripts/Fedora/rpmfusion_repos.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/sh
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
